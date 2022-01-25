d62fec9d99e2342e3f66afe85228ec5911b842fd
except134 <except134@gmail.com>
2022-01-25 19:32:12 +0300

    Lesson 1, task 1 finished

---
+++ Lesson1/mainwindow.h
@@ -0,0 +1,27 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtMath>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_btnTask1Calc_pressed();
+
+    void on_edTask1Result_textChanged(const QString &arg1);
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
